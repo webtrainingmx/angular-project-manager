2f00a84f5ce52ed83c09ba716d6e709b7ce5db33
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-05-29 15:34:46 -0500

    Usar issues list y service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,22 @@ import { HeaderComponent } from './common/layout/header/header.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 
 import { ProjectListService } from './project-list/services/project-list.service';
+import { IssuesListService } from './issues-list/services/issues-list.service';
+import { IssuesListComponent } from './issues-list/issues-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    IssuesListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [ProjectListService],
+  providers: [ProjectListService, IssuesListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
