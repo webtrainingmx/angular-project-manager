9f72523af80644ff0e67e352d6c8c75aa3382175
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-05-29 15:22:41 -0500

    Usar componentes header y project list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/layout/header/header.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+
+import { ProjectListService } from './project-list/services/project-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProjectListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
