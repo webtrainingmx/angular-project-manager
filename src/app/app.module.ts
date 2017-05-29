0541a78c0365db12173dbef93a3e277e84d6cfaa
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-05-29 16:24:06 -0500

    Incluir router module y ejemplo de ruta padre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/layout/header/header.component';
@@ -10,17 +11,25 @@ import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectListService } from './project-list/services/project-list.service';
 import { IssuesListService } from './issues-list/services/issues-list.service';
 import { IssuesListComponent } from './issues-list/issues-list.component';
+import { HomeComponent } from './auth/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, pathMatch: 'full'
+  } ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProjectListComponent,
-    IssuesListComponent
+    IssuesListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     HttpModule
   ],
   providers: [ProjectListService, IssuesListService],
