67052be43f998216b2f252ae422b574442fddfc3
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-05-29 16:25:17 -0500

    Mover componentes a zona autenticada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/layout/header/header.component';
-import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectListComponent } from './auth/project-list/project-list.component';
 
-import { ProjectListService } from './project-list/services/project-list.service';
-import { IssuesListService } from './issues-list/services/issues-list.service';
-import { IssuesListComponent } from './issues-list/issues-list.component';
+import { ProjectListService } from './auth/project-list/services/project-list.service';
+import { IssuesListService } from './auth/issues-list/services/issues-list.service';
+import { IssuesListComponent } from './auth/issues-list/issues-list.component';
 import { HomeComponent } from './auth/home/home.component';
 
 const routes: Routes = [
