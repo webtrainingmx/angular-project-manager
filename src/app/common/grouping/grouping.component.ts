1d18516da6a15dcd91ab8d522faeaa0a9149a21c
Alex Serrano <alexserrano63@gmail.com>
2017-06-02 10:31:13 -0500

    WIP: Add grouping component

---
+++ src/app/common/grouping/grouping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grouping',
+  templateUrl: './grouping.component.html',
+  styleUrls: ['./grouping.component.css']
+})
+export class GroupingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
