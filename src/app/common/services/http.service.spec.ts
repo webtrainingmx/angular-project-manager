84dd4ed2f72d43f3e2c4d2b5e69c20b1e63a7a16
Alex Serrano <alexserrano63@gmail.com>
2017-06-02 10:31:28 -0500

    Add http services

---
+++ src/app/common/services/http.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { HttpService } from './http.service';
+
+describe('HttpService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [HttpService]
+    });
+  });
+
+  it('should ...', inject([HttpService], (service: HttpService) => {
+    expect(service).toBeTruthy();
+  }));
+});
