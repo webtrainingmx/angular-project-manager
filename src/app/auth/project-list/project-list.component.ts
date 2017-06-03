518bf9bfef6672136fb794fa892200c3ab641e23
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-06-02 20:05:45 -0500

    Agregar soporte para eliminación de proyectos

--- src/app/auth/project-list/project-list.component.ts
+++ src/app/auth/project-list/project-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectListService } from './services/project-list.service';
 import { Project } from './models/project.model';
+import { HttpService } from '../../common/services/http.service';
+import {Config} from '../../common/config';
 
 @Component({
   selector: 'app-project-list',
@@ -10,10 +12,16 @@ import { Project } from './models/project.model';
 export class ProjectListComponent implements OnInit {
 
   projects: Array<Project> = [];
+  apiBaseURL: string = Config.API_SERVER_URL;
 
-  constructor(private _projectListService: ProjectListService) { }
+  constructor(private _projectListService: ProjectListService,
+  private _httpService: HttpService) { }
 
   ngOnInit() {
+    this.getAllProjects();
+  }
+
+  getAllProjects() {
     this._projectListService.getAll().subscribe((projects: Project[] = []) => {
         this.projects = projects;
       },
@@ -24,10 +32,21 @@ export class ProjectListComponent implements OnInit {
         console.log('Finished!');
       });
   }
-
   public setData(sortedData) {
     console.log('sortedData: %o', sortedData);
     this.projects = sortedData;
+  }
+
+  onDeleteProject(project: Project) {
+    const url = `${this.apiBaseURL}/projects/${project.id}`;
+
+    this._httpService.delete(url).subscribe((response) => {
+        console.log(response);
+        this.getAllProjects();
+      },
+      err => {
+        console.error(err);
+      });
   }
 
 }
